2b172977c7c8c2c8cecd0050571b57ffab3bc553
oarcenth <60490706+oarcenth@users.noreply.github.com>
2020-02-06 14:31:07 -0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,155 @@
+// STEP 1 
+/*
+var someMonth;
+function theMonth();
+var currentMonth;
+var summerMonth;
+var myLibraryFunction; 
+*/
+
+//STEP 2 
+/*
+var myAge = 27;
+var myName = "Omar";
+var summerMonth = false;
+var summerDay = null;
+*/
+
+// STEP 3 
+/*
+var myName = myFirstName + myLastName; 
+var myAddress = addressNumber + streetName + zipCode + stateName; 
+*/
+
+// STEP 4 
+/*
+let firstName; 
+let lastName;
+let address;
+let city; 
+let state;
+let zipCode; 
+let yourAge;
+let referralSource;
+let mayWeContactYou;
+*/
+
+// STEP 5
+
+
+
+// STEP 6 
+/*
+let myAge = 27; 
+window.console.log("I am " + myAge + " years old.");
+*/
+
+// STEP 7 
+/*
+let goodTime = true; 
+window.console.log("Are you having a good time? " + goodTime);
+let myAge = true; 
+window.console.log(myAge + 24);
+*/
+
+// STEP 8 (NOT A VALID. STATEMENT SEE BELOW)
+/*
+var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+window.console.log(someString);
+*/
+
+// STEP 9 
+/*
+let myTime = null;
+window.console.log(myTime);
+let yourName; 
+window.console.log(yourName); 
+*/
+
+// STEP 10 
+/*
+let myName = "Omar";
+let myAge = 27;
+let tired = "true";
+let married = null;
+window.console.log(typeof myName);
+window.console.log(typeof myAge);
+window.console.log(typeof tired);
+window.console.log(typeof married);
+window.console.log(typeof favoriteBand);
+*/
+
+// STEP 11
+/*
+window.alert("Hello " + "Omar Arce" + ", welcome to the JavaScript Class!"); 
+*/
+
+// STEP 12 
+/*
+let name = "Omar Arce";
+window.alert("Hello " + name + ", welcome to the JavaScript Class!"); 
+*/
+
+// STEP 13
+/*
+let name = "Omar Arce";
+let course = "JavaScript"; 
+window.alert("Hello" + name + ", welcome to the " + course + " class!"); 
+*/
+
+// STEPS 14
+/*
+window.alert("Hello Zak Ruvalcaba. \nWelcome to the Javascript class!");
+*/
+
+// STEPS 15  
+
+
+// STEPS 16
+
+
+// STEP 17 
+/*
+var x = 10;
+var y = 20;
+window.console.log(x +y);
+*/
+
+// STEP 18
+/*
+var x = 20;
+window.console.log(x + 20); 
+*/
+
+// STEP 19
+/*
+var x = 20;
+window.console.log(x * 5);
+*/
+
+// STEP 20
+/*
+var x = 20/3;
+*/
+
+// STEP 21
+/*
+let x = 45;
+let y = 55;
+window.console.log(x == 45 && y == 55);
+*/
+
+//STEP 22 
+/*
+let x = 45; 
+let y = 55; 
+window.console.log(x > y || x >= 46); 
+*/
+
+// STEP 23 
+
+
+// STEP 24
+
+
+// STEP 25
